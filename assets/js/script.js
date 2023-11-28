d3468f390e7cf3f06fb6043cd7d27e5500cbf542
Daniel Turner <lunartechfreek@googlemail.com>
2023-11-28 19:16:58 +0000

    Add initial code to display game and how to sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let introButtons = document.getElementsByClassName('intro-btn');
+    let welcomeBox = document.getElementsByClassName('welcome')[0];
+    let howToBox = document.getElementsByClassName('how-to')[0];
+    let questionBox = document.getElementsByClassName('question-box')[0];
+    let scoreBox = document.getElementsByClassName('score-area')[0];
+
+    for (let introbutton of introButtons) {
+        introbutton.addEventListener('click', function () {
+            if (introbutton.id === 'how-to-btn') {
+                welcomeBox.classList.add('hide');
+                howToBox.classList.remove('hide');
+            } else {
+                welcomeBox.classList.add('hide');
+                questionBox.classList.remove('hide');
+                scoreBox.classList.remove('hide');
+            }
+        });
+    }
+});
+
+
+
 function runGame() {
 
 }
@@ -5,3 +28,4 @@ function runGame() {
 function nextQuestion() {
 
 }
+
